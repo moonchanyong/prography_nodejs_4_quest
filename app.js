a7504596fd1531ec7fa77a07b84748927e6e8073
chanyong.moon <chanyong.moon@navercorp.com>
2019-02-18 20:16:34 +0900

    fix: use es6

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var run_crawlR = require('./routes/run_crawl');
-var listR = require('./routes/list');
+const run_crawlR = require('./routes/run_crawl');
+const listR = require('./routes/list');
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
