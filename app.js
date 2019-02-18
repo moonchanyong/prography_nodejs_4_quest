b9e1f5b70e64a95ec7d13d636a8db7b00f176044
chanyong.moon <chanyong.moon@navercorp.com>
2019-02-18 19:59:38 +0900

    feature: change route name

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var run_crawlR = require('./routes/run_crawl');
+var listR = require('./routes/list');
 
 var app = express();
 
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/run_crawl', run_crawlR);
+app.use('/list', listR);
 
 module.exports = app;
